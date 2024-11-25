ba87a11691b729a31ffe4dda719c621589484987
tomashined <tomashined@gmail.com>
2024-11-25 18:25:29 -0500

    Update ReplayManager.cs

--- Assets/Video/ReplayManager.cs
+++ Assets/Video/ReplayManager.cs
@@ -13,14 +13,21 @@ namespace Video
         
         [Header("Parameters")]
         [SerializeField] private string videoPath;
-        [SerializeField] private int timestampMs;
+
+        [Header("Time")]
+        [SerializeField] private float durationSeconds;
+        [SerializeField] private float durationMs;
+        [SerializeField] private long timestampNs;
+        [SerializeField] private float timestampMs;
 
         [Header("State")]
+        [SerializeField] private long startTimestampNs;
         [SerializeField] private int width;
         [SerializeField] private int height;
         [SerializeField] private float frameRate;
-        [SerializeField] private int durationMs;
         [SerializeField] private Texture2D texture;
+
+        private List<string> timestamps;
         
         private async Task LoadVideo(Action<Texture2D> onSetTexture)
         {
@@ -32,7 +39,8 @@ namespace Video
             width = mediaAsset.width;
             height = mediaAsset.height;
             frameRate = mediaAsset.frameRate;
-            durationMs = (int)(mediaAsset.duration * 1000);
+            durationSeconds = mediaAsset.duration;
+            durationMs = durationSeconds * 1000;
             Debug.Log($"{width}x{height} @{frameRate}Hz {mediaAsset.duration}s");
             
             // Create and display texture
@@ -44,12 +52,14 @@ namespace Video
         {
             await LoadVideo(onSetTexture);
             
+            timestamps = new List<string>();
             // Read pixel buffers
             foreach (var pixelBuffer in mediaAsset.Read<PixelBuffer>()) {
                 // Copy pixel data into the texture
                 var textureBuffer = new PixelBuffer(texture);
                 pixelBuffer.CopyTo(textureBuffer);
-                timestampMs = (int)pixelBuffer.timestamp;
+                timestampNs = pixelBuffer.timestamp;
+                timestamps.Add($"{timestampNs}");
                 // Upload the texture data to the GPU
                 texture.Apply();
                 // Wait for next frame
@@ -57,17 +67,89 @@ namespace Video
             }
             // Log
             Debug.Log($"Finished reading frames");
+            await File.WriteAllLinesAsync(Path.Combine(Application.persistentDataPath, "timestamps.txt"), timestamps);
         }
         public async void OnFindButtonPressed(Action<Texture2D> onSetTexture)
         {
             await LoadVideo(onSetTexture);
             
             timestampMs = Mathf.Clamp(timestampMs, 0, durationMs);
+            timestampNs = (long)(timestampMs * 1000000f);
+            await Task.Yield();
+
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            foreach (var pixelBuffer in mediaAsset.Read<PixelBuffer>())
+            {
+                if (pixelBuffer.timestamp >= timestampNs)
+                {
+                    var textureBuffer = new PixelBuffer(texture);
+                    pixelBuffer.CopyTo(textureBuffer);
+                    timestampNs = pixelBuffer.timestamp;
+                    // Upload the texture data to the GPU
+                    texture.Apply();
+                    
+                    stopwatch.Stop();
+                    Debug.Log($"Frame at {timestampMs}ms is {timestampNs} after {stopwatch.ElapsedMilliseconds}ms");
+                    
+                    break;
+                }
+            }
+        }
+        public async void OnFindButtonPressedAndRecord(Action<Texture2D> onSetTexture)
+        {
+            await LoadVideo(onSetTexture);
             
+            timestampMs = Mathf.Clamp(timestampMs, 0, durationMs);
+            timestampNs = (long)(timestampMs * 1000000f);
+            await Task.Yield();
+            
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            
+            var recorder = await MediaRecorder.Create(
+                format: MediaRecorder.Format.MP4,
+                width: width,
+                height: height,
+                frameRate: frameRate
+                );
+            startTimestampNs = -1;
+            foreach (var pixelBuffer in mediaAsset.Read<PixelBuffer>())
+            {
+                if (pixelBuffer.timestamp >= timestampNs)
+                {
+                    var textureBuffer = new PixelBuffer(texture);
+                    pixelBuffer.CopyTo(textureBuffer);
+                    timestampNs = pixelBuffer.timestamp;
+                    // Upload the texture data to the GPU
+                    texture.Apply();
+                    if (startTimestampNs < 0) startTimestampNs = timestampNs;
+                    using var newpixelBuffer = new PixelBuffer(
+                        texture,            // texture to get pixel data from
+                        timestampNs - startTimestampNs   // pixel buffer timestamp to use for recording
+                        );
+                    
+                    stopwatch.Stop();
+                    //Debug.Log($"Frame at {timestampMs}ms is {timestampNs} after {stopwatch.ElapsedMilliseconds}ms");
+                    
+                    recorder.Append(newpixelBuffer);
+                }
+            }
+            MediaAsset asset = await recorder.FinishWriting();
+            Debug.Log($"Recorded {asset.width}x{asset.height}@{asset.frameRate} - {asset.path}");
+        }
+        
+        public async void OnFindButtonPressed1(Action<Texture2D> onSetTexture)
+        {
+            await LoadVideo(onSetTexture);
+            
+            timestampMs = Mathf.Clamp(timestampMs, 0, durationMs);
+            timestampNs = (long)(timestampMs * 1000000f);
+            await Task.Yield();
+            
+            var pixelBuffer = mediaAsset.Read<PixelBuffer>().ToList().Find(buffer => buffer.timestamp >= timestampNs);
             var textureBuffer = new PixelBuffer(texture);
-            var pixelBuffer = mediaAsset.Read<PixelBuffer>().First(buffer => buffer.timestamp >= timestampMs);
-            Debug.Log($"Frame at {timestampMs} is {pixelBuffer.timestamp}");
             pixelBuffer.CopyTo(textureBuffer);
+            timestampNs = pixelBuffer.timestamp;
+            // Upload the texture data to the GPU
             texture.Apply();
         }
         public void OnFindFieldChanged(int evtNewValue)
